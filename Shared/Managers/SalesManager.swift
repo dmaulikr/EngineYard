a5635dcd5778d125a81417b62ba8c8eb12b2659f
worchyld <username@users.noreply.github.com>
2017-07-24 20:24:16 +0100

    Start creation sales manager

---
+++ Shared/Managers/SalesManager.swift
@@ -0,0 +1,13 @@
+//
+//  SalesManager.swift
+//  EngineYard
+//
+//  Created by Amarjit on 24/07/2017.
+//  Copyright © 2017 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+struct SalesManager {
+    
+}
