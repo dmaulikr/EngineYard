fb45436899170bef9865a3d194b41216edde79ba
worchyld <username@users.noreply.github.com>
2017-07-25 17:19:17 +0100

    Update main menu view controller

--- EngineYard_iOS/ViewControllers/MainMenuViewController.swift
+++ EngineYard_iOS/ViewControllers/MainMenuViewController.swift
@@ -2,24 +2,59 @@
 //  MainMenuViewController.swift
 //  EngineYard
 //
-//  Created by Amarjit on 20/07/2017.
+//  Created by Amarjit on 25/07/2017.
 //  Copyright © 2017 Amarjit. All rights reserved.
 //
 
 import UIKit
 
+struct MainMenuViewModel {
+    enum MainMenuTag: Int {
+        case newGameTag = 0
+    }
+
+    func menuButtonPressed(tag: Int) {
+        guard let tagSelected = MainMenuTag.init(rawValue: tag) else {
+            return
+        }
+        switch tagSelected {
+        case .newGameTag:
+            /*
+            let sb : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc : NewGameViewController = sb.instantiateInitialViewController() as! NewGameViewController
+            navigationController.pushViewController(vc, animated: true)
+             */
+            break
+        }
+    }
+}
+
 class MainMenuViewController: UIViewController {
+
+    @IBOutlet var mainMenuBtnOutletCollection: [UIButton]!
+
+    var viewModel : MainMenuViewModel = MainMenuViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mainMenuBtnPressed(_ sender: UIButton) {
+        self.viewModel.menuButtonPressed(tag: (sender).tag)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
