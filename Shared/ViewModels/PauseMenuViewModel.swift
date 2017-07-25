8df8a842ea4b43473da57cc9fdc9ee0d30767488
worchyld <username@users.noreply.github.com>
2017-07-25 13:45:49 +0100

    add view model

---
+++ Shared/ViewModels/PauseMenuViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  PauseMenuViewModel.swift
+//  EngineYard
+//
+//  Created by Amarjit on 25/07/2017.
+//  Copyright © 2017 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+class PauseViewModel {
+
+    func continueGame()
+    {
+
+    }
+
+    func quitGame() {
+        // are you sure?
+        // if true: end game
+    }
+
+}
