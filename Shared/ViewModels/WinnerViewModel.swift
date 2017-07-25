fb3072f8b3a0e79e1a29023511f79d0b6541a27c
worchyld <username@users.noreply.github.com>
2017-07-25 13:41:30 +0100

    Add basic winner view model

---
+++ Shared/ViewModels/WinnerViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  WinnerViewModel.swift
+//  EngineYard
+//
+//  Created by Amarjit on 25/07/2017.
+//  Copyright © 2017 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+class WinnerViewModel
+{
+    var game: Game!
+    lazy var winners: [Player]?
+
+
+}
