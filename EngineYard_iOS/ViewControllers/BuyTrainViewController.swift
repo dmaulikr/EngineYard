e974048bc2635f50f24d69e9830d20b42fafa471
worchyld <username@users.noreply.github.com>
2017-07-26 15:59:20 +0100

    Add child controller and a common train page

--- EngineYard_iOS/ViewControllers/BuyTrainViewController.swift
+++ EngineYard_iOS/ViewControllers/BuyTrainViewController.swift
@@ -10,21 +10,57 @@ import UIKit
 
 class BuyTrainViewController: UIViewController {
 
-    //weak var HUD: HUDViewController?
+    weak var controller: TrainViewController?
+
+    deinit {
+        guard let hasChildController = self.controller else {
+            return
+        }
+        hasChildController.removeFromParentViewController()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //self.HUD = HUDViewController.loadHUD(game: nil, viewController: self)
+        let sb: UIStoryboard = UIStoryboard(name: "Train", bundle: nil)
+        if let controller = sb.instantiateViewController(withIdentifier: "TrainViewController") as? TrainViewController
+        {
+            self.controller = controller
+            addChildViewController(controller)
+            controller.view.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(controller.view)
+
+            NSLayoutConstraint.activate([
+                controller.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                controller.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+                controller.view.topAnchor.constraint(equalTo: self.view.topAnchor),
+                controller.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+                ])
+
+            controller.didMove(toParentViewController: self)
+
+            controller.completionClosure = { (doneBtnPressed: Bool) in
+                // end turn, handle whether to move to next page
+                self.moveToProductionSegue()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
+    func shouldMoveToProductionSegue() -> Bool {
+        return true
+    }
+
+    func moveToProductionSegue() {
+        if (shouldMoveToProductionSegue()) {
+            self.performSegue(withIdentifier: "productionSegue", sender: self)
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,6 +68,6 @@ class BuyTrainViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
