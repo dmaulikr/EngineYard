411d1c29f5f6bc8ddba922ca77f31f1fea1b4a2c
AD <silentflute.ad@gmail.com>
2017-07-30 23:09:49 +0100

    add test

---
+++ EngineYardTests/ProductionTests.swift
@@ -0,0 +1,25 @@
+//
+//  ProductionTests.swift
+//  EngineYard
+//
+//  Created by Amarjit on 30/07/2017.
+//  Copyright © 2017 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+class ProductionTests: BaseTests {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}
