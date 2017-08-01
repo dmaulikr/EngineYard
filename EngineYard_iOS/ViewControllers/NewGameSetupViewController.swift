349e0a02d34ed9439432ae72bc3b463ba959529a
AD <silentflute.ad@gmail.com>
2017-08-01 21:53:32 +0100

    fixed bugs on the stepper

--- EngineYard_iOS/ViewControllers/NewGameSetupViewController.swift
+++ EngineYard_iOS/ViewControllers/NewGameSetupViewController.swift
@@ -9,42 +9,26 @@
 import UIKit
 
 protocol StepperProtocol {
-    func updateStepperText()
+    func updateStepperLabel(with text: String)
 }
-
 
 class NewGameViewModel
 {
     var delegate: StepperProtocol?
 
-    var players: [Player]?
-    var stepperValue: Int = Constants.NumberOfPlayers.max {
+    private(set) var players: [Player] = [Player]()
+
+    public var stepperValue: Int = Constants.NumberOfPlayers.max {
         didSet {
-            delegate?.updateStepperText()
+            let text = NSLocalizedString("\(self.stepperValue) Players", comment: "Label for number of players in game")
+            delegate?.updateStepperLabel(with: text)
         }
     }
 
-    private(set) var playersCopy:[Player] = [Player]()
+    init() {
+        self.stepperValue = Constants.NumberOfPlayers.max
 
-    lazy var numberOfItemsInSection : Int = {
-        guard let players = self.players else {
-            return 0
-        }
-        return players.count
-    }()
-
-    lazy var stepperLabelText: String = {
-        guard let hasPlayers = self.players else {
-            return "- No players defined -"
-        }
-        return NSLocalizedString("\(hasPlayers.count) Players", comment: "Label for number of players in game")
-    }()
-
-
-    init(playerCount:Int = Constants.NumberOfPlayers.max) {
-        self.stepperValue = playerCount
-
-        for idx:Int in 1...playerCount {
+        for idx:Int in 1...self.stepperValue {
             let name = "Player #\(idx)"
             let filename = "avt_\(idx)"
 
@@ -54,66 +38,28 @@ class NewGameViewModel
             }
 
             let playerObj = Player.init(name: name, isAI: isAI, asset: filename)
-            self.players?.append(playerObj)
+            self.players.append(playerObj)
         }
-
-        print ("#: \(self.players?.count) players")
     }
 
     func configureCell(cell: AddPlayerCollectionViewCell, atIndexPath: IndexPath) {
-
-        guard let playerList = self.players else {
-            return
-        }
-
-        let playerObj = playerList[atIndexPath.row]
+        let playerObj = self.players[atIndexPath.row]
 
         if let avatar = UIImage(named: playerObj.asset) {
             cell.avtBtn.setImage(avatar, for: .normal)
         }
         cell.aiSwitchBtn.isSelected = playerObj.isAI
+
     }
 
     func stepperValueDidChange(value: Int) {
-        if value > self.stepperValue {
-            self.addPlayer()
-        }
-        else {
-            self.removePlayer()
-        }
         self.stepperValue = value
-    }
-
-    func addPlayer() {
-
-        guard let hasPlayers = self.players else {
-            return
-        }
-
-        if (hasPlayers.count < Constants.NumberOfPlayers.max) {
-            if let lastPlayerCopy = playersCopy.last {
-                self.players?.append(lastPlayerCopy)
-                self.playersCopy.removeLast()
-            }
-        }
-    }
-
-    func removePlayer() {
-        guard var hasPlayers = self.players else {
-            return
-        }
-
-        if (hasPlayers.count > Constants.NumberOfPlayers.min) {
-            if let lastPlayer = hasPlayers.last {
-                self.playersCopy.append(lastPlayer)
-                hasPlayers.removeLast()
-            }
-        }
     }
 }
 
 class NewGameSetupViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, StepperProtocol
 {
+
     var viewModel: NewGameViewModel?
 
     @IBOutlet weak var stepper: UIStepper!
@@ -123,6 +69,8 @@ class NewGameSetupViewController: UIViewController, UICollectionViewDelegate, UI
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.viewModel = NewGameViewModel.init()
 
         self.stepper.minimumValue = Double(Constants.NumberOfPlayers.min)
         self.stepper.maximumValue = Double(Constants.NumberOfPlayers.max)
@@ -142,25 +90,26 @@ class NewGameSetupViewController: UIViewController, UICollectionViewDelegate, UI
     // MARK: - IBActions
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-
+        self.viewModel?.stepperValueDidChange(value: Int(sender.value))
+        self.collectionView.reloadData()
     }
 
     @IBAction func doneBtnPressed(_ sender: UIButton) {
 
     }
 
-    internal func updateStepperText() {
-        self.stepperLabel.text = self.viewModel?.stepperLabelText
+    internal func updateStepperLabel(with text: String) {
+        self.stepperLabel.text = text
         self.stepperLabel.layoutIfNeeded()
     }
 
     // MARK: - Collection View
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        guard let numberOfItems = self.viewModel?.numberOfItemsInSection else {
+        guard let value = self.viewModel?.stepperValue else {
             return 0
         }
-        return numberOfItems
+        return value
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
