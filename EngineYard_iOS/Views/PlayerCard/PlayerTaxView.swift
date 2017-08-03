d7aebff19481d5c84f23a12f1f3101346e899da9
AD <silentflute.ad@gmail.com>
2017-08-03 16:47:52 +0100

    Update tax view model

--- EngineYard_iOS/Views/PlayerCard/PlayerTaxView.swift
+++ EngineYard_iOS/Views/PlayerCard/PlayerTaxView.swift
@@ -8,14 +8,58 @@
 
 import UIKit
 
+fileprivate struct PlayerTaxViewModel
+{
+    public static var circleRadius: CGFloat = 70.0
+
+    public private(set) var preTaxBalance: Int = 0 {
+        didSet {
+            self.taxDue = Tax.calculateTaxDue(onBalance: self.preTaxBalance)
+        }
+    }
+    public private(set) var taxDue: Int = 0
+    public var balance: Int {
+        return Int(self.preTaxBalance - taxDue)
+    }
+
+    lazy var formattedPreTaxBalance : String? = {
+        let number = NSNumber(integerLiteral: self.preTaxBalance)
+        guard let formatted = ObjectCache.currencyRateFormatter.string(from: number) else {
+            return String(0)
+        }
+        return formatted
+    }()
+
+    lazy var formattedTaxDue : String = {
+        let number = NSNumber(integerLiteral: self.taxDue)
+        guard let formatted = ObjectCache.currencyRateFormatter.string(from: number) else {
+            return String(0)
+        }
+        return formatted
+    }()
+
+    lazy var formattedBalance: String = {
+        let number = NSNumber(integerLiteral: self.balance)
+        guard let formatted = ObjectCache.currencyRateFormatter.string(from: number) else {
+            return String(0)
+        }
+        return formatted
+    }()
+
+    init(player: Player) {
+        self.preTaxBalance = player.wallet.balance
+    }
+}
+
 class PlayerTaxView: UIView {
 
     @IBOutlet weak var headerBGView: UIView!
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet var taxLabelOutletCollection: [UILabel]!
+
     @IBOutlet weak var preTaxLabel: UILabel!
+    @IBOutlet weak var taxDueLabel: UILabel!
     @IBOutlet weak var balanceLabel: UILabel!
-    @IBOutlet weak var taxDueAmountLabel: UILabel!
 
 
     func setup(player: Player) {
@@ -23,29 +67,15 @@ class PlayerTaxView: UIView {
         guard let image = UIImage(named: player.asset) else {
             return
         }
-        let balance: Int = player.wallet.balance
-        let taxDue: Int = Tax.calculateTaxDue(onBalance: player.wallet.balance)
-        let newBalance: Int = Int(balance - taxDue)
 
-        let preTaxNumber = NSNumber(integerLiteral: balance)
-        let taxDueNumber = NSNumber(integerLiteral: taxDue)
-        let balanceNumber = NSNumber(integerLiteral: newBalance)
+        var taxViewModel: PlayerTaxViewModel = PlayerTaxViewModel(player: player)
 
-        guard let preTaxNumberFormatted = ObjectCache.currencyRateFormatter.string(from: preTaxNumber) else {
-            return
-        }
-        guard let taxDueNumberFormatted = ObjectCache.currencyRateFormatter.string(from: taxDueNumber) else {
-            return
-        }
-        guard let balanceNumberFormatted = ObjectCache.currencyRateFormatter.string(from: balanceNumber) else {
-            return
-        }
+        self.avatarImageView.image = image.circled(forRadius: PlayerTaxViewModel.circleRadius)
+        self.avatarImageView.layoutIfNeeded()
 
-        self.avatarImageView.image = image.circled(forRadius: 70)
-
-        self.preTaxLabel.text = preTaxNumberFormatted
-        self.taxDueAmountLabel.text = taxDueNumberFormatted
-        self.balanceLabel.text = balanceNumberFormatted
+        self.preTaxLabel.text = taxViewModel.formattedPreTaxBalance
+        self.taxDueLabel.text = taxViewModel.formattedTaxDue
+        self.balanceLabel.text = taxViewModel.formattedBalance
 
         let _ = self.taxLabelOutletCollection.map({
             $0.sizeToFit()
