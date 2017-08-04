fc648340fd7b9422ed7914cb2067dc1dc35ff9d1
AD <silentflute.ad@gmail.com>
2017-08-04 09:05:59 +0100

    remove un-necessary icons and add table view cell

---
+++ EngineYard_iOS/Views/EngineCard/RivalTrainOwnersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RivalTrainOwnersTableViewCell.swift
+//  EngineYard
+//
+//  Created by Amarjit on 04/08/2017.
+//  Copyright © 2017 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class RivalTrainOwnersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
